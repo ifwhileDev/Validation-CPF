f590567949a9270c68d2aaece61a25a7e8b986aa
mat <92649949+ifwhileDev@users.noreply.github.com>
2025-10-04 02:55:31 -0300

    Create verify.py

---
+++ verify.py
@@ -0,0 +1,28 @@
+from random import randint
+
+def cpf_validate(numbers):
+    cpf = [int(char) for char in numbers if char.isdigit()]
+
+    if len(cpf) != 11:
+        return False
+
+    if cpf == cpf[::-1]:
+        return False
+
+    for i in range(9, 11):
+        value = sum((cpf[num] * ((i+1) - num) for num in range(0, i)))
+        digit = ((value * 10) % 11) % 10
+        if digit != cpf[i]:
+            return False
+    return True
+
+
+print("-=" * 20)
+print("CPF VALIDATION")
+print("-=" * 20)
+cpf = input('Digite o CPF: ')
+if cpf_validate(cpf):
+    print('CPF válido.')
+else:
+    print('CPF inválido.')
+
